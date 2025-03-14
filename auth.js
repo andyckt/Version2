7fae8ed52f92593e12cefd3fd52a6cc530b43184
andyckt <kamtocheung1104@gmail.com>
2025-03-14 22:32:51 +0800

    Add basic authentication functions

---
+++ auth.js
@@ -0,0 +1,13 @@
+// User Authentication Module
+function login(username, password) {
+    // Login logic here
+}
+
+function register(username, password, email) {
+    // Registration logic here
+}
+
+module.exports = {
+    login,
+    register
+}; 
